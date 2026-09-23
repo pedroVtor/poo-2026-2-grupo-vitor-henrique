652b1a58756a4481c852cf750130da23e3f23317
pedroVtor <alanzokapq123@gmail.com>
2026-09-23 19:54:31 -0300

    Create Guerreiro.java

---
+++ src/Guerreiro.java
@@ -0,0 +1,34 @@
+public class Guerreiro extends Personagem {
+    private int defesa;
+
+    public Guerreiro(String nome) {
+        super(nome);
+        setDefesa(5);
+    }
+
+    public Guerreiro(String nome, int vida, int forca) {
+        super(nome, vida, forca);
+        setDefesa(5);
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public void setDefesa(int defesa) {
+        if (defesa < 0) {
+            throw new IllegalArgumentException("defesa não pode ser negativa");
+        }
+        this.defesa = defesa;
+    }
+
+    @Override
+    public void receberDano(int dano) {
+        super.receberDano(Math.max(0, dano - defesa));
+    }
+
+    @Override
+    public String habilidade() {
+        return getNome() + " realizou um golpe poderoso!";
+    }
+}
